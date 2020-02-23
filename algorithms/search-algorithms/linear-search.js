15f441fa0b0c460a3e39083a4fe6055872a67cbc
Grace Sekwao <grace@nentgroup.com>
2020-02-23 20:18:12 +0100

    add linear search

---
+++ algorithms/search-algorithms/linear-search.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+
+    function foo (arr, x) {
+       for (i=0; i < arr.length; i++) {
+           if(arr[i] === x) {
+               return i
+           }
+       }
+       return -1;
+
+    }
+    res.end('hello');
+  });
+  
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
