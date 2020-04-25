5dbc67830eed8ce0f162b32f7d23bf3e4b1a6a55
Grace Sekwao <grace@nentgroup.com>
2020-04-25 09:22:00 +0200

    fix fille issues again

---
+++ README.md
@@ -0,0 +1,1 @@
+# This repo is for various algorithms using Javascript
---
+++ algorithms/miscellenous/binary-search-nearly-sorted.js
@@ -0,0 +1,36 @@
+function foo (arr, x) {
+    if (!arr.length || !arr) {
+        throw 'array cannot be empty';
+    }
+
+    let start = 0;
+    let end = arr.length;
+
+    try {
+        while(start <= end) {
+            let midIndex = Math.floor(start + ((end - start)/2));
+            console.log(arr[midIndex]);
+            if(arr[midIndex] === x){
+                return midIndex;
+            
+            } else if(midIndex > start && arr[midIndex - 1] === x) {
+                return midIndex - 1;
+            } else if(midIndex < end && arr[midIndex + 1] === x) {
+                return midIndex + 1;
+            }
+            else if(arr[midIndex] > x) {
+                end = midIndex - 1;
+            } else {
+                start = midIndex + 1;
+            }
+        }
+    
+        return `element ${x} not found`;
+    } catch(e) {
+        console.log(e);
+    }
+  
+}
+
+console.log(foo([10,2,3,4,5,6], 3))
+
